c634ce07b5806450c9fe0f4943a8145374c50aa8
Michael Praschl <michael.praschl@gmail.com>
2017-09-28 23:41:26 +0200

    Add project files.

---
+++ MiP.TeamBuilds/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace MiP.TeamBuilds
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MiP.TeamBuilds/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace MiP.TeamBuilds
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BuildDefinitions_Click(object sender, RoutedEventArgs e)
+        {
+            mainView.Visibility = Visibility.Visible;
+        }
+
+        private void Builds_Click(object sender, RoutedEventArgs e)
+        {
+            mainView.Visibility = Visibility.Visible;
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            mainView.Visibility = Visibility.Collapsed;
+            settingsView.Visibility = Visibility.Visible;
+        }
+
+        private void SettingsApply_Click(object sender, RoutedEventArgs e)
+        {
+            mainView.Visibility = Visibility.Visible;
+            settingsView.Visibility = Visibility.Collapsed;
+        }
+    }
+}
