7ec725f68ebc682cbf50d37270ebd156c727242e
Michael Praschl <michael.praschl@gmail.com>
2017-11-08 20:43:57 +0100

    is now a single instance application

--- MiP.TeamBuilds/App.xaml.cs
+++ MiP.TeamBuilds/App.xaml.cs
@@ -1,19 +1,48 @@
 ﻿using Autofac;
+using Microsoft.Shell;
 using MiP.TeamBuilds.IoC;
 using System.Windows;
+using System.Collections.Generic;
+using MiP.TeamBuilds.UI.Main;
 
 namespace MiP.TeamBuilds
 {
-    public partial class App : Application
+    public partial class App : Application, ISingleInstanceApp
     {
+        private const string UniqueApplicationKey = "MiP.TeamBuilds.App.Unique_997ab5db-3580-4446-b16e-58cf2acdc9f6";
+
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
-            // TODO: Allow only one instance of Application running
+            if (SingleInstance<App>.InitializeAsFirstInstance(UniqueApplicationKey))
+            {
+                var window = ServiceLocator.Instance.Resolve<MainWindow>();
 
-            var window = ServiceLocator.Instance.Resolve<UI.Main.MainWindow>();
+                // start in hidden mode
+                window.Hide();
+            }
+            else
+            {
+                Shutdown();
+            }
+        }
 
-            // start in hidden mode
-            window.Hide();
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            SingleInstance<App>.Cleanup();
+        }
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            // application was opened a second time.
+            // this method is called in the first instance of the application with the args that were passed to the second.
+
+            // lets bring the main window into view, when another instance was started
+
+            //App.Current.MainWindow.WindowState = WindowState.Normal;
+            //App.Current.MainWindow.Visibility = Visibility.Visible;
+            //App.Current.MainWindow.BringIntoView();
+
+            return true;
         }
     }
 }
