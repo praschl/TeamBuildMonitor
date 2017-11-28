7fa71660fb46c4ac6ecac45cda981a490122f1a4
Michael Praschl <michael.praschl@gmail.com>
2017-11-28 23:20:09 +0100

    overview can now sort

--- MiP.TeamBuilds/UI/Overview/OverviewViewModel.cs
+++ MiP.TeamBuilds/UI/Overview/OverviewViewModel.cs
@@ -1,28 +1,29 @@
 ﻿using MiP.TeamBuilds.Providers;
 using MiP.TeamBuilds.UI.Notifications;
-using PropertyChanged;
 using System.ComponentModel;
 using System.Windows.Data;
 using System.Windows.Input;
+using System;
+using System.Linq;
 
 namespace MiP.TeamBuilds.UI.Overview
 {
     public class OverviewViewModel : INotifyPropertyChanged
     {
-        // TODO: Overview: Implement sorting (SortCommand)
         // TODO: Overview: Filter builds by text + Label "Showing 17 / 239 builds"
         // TODO: Overview: Second Listview for finished builds
         // TODO: Overview: Menu for Droplocation, Buildsummary
         // TODO: Overview: Display progress based on older known builds
         // TODO: Overview: Menu for Stop build, Retry build
+        // TODO: Overview: Display direction of sort in list headers
 
         public KnownBuildsViewModel KnownBuildsViewModel { get; set; }
-
-        public ICommand SortCommand => null;
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ICollectionView BuildsView { get; set; }
+
+        public ICommand SortCommand { get; set; }
 
         public OverviewViewModel(KnownBuildsViewModel knownBuildsViewModel)
         {
@@ -34,11 +35,45 @@ namespace MiP.TeamBuilds.UI.Overview
              * for the same collection. So, to use different collection views (for different controls) on the same collection instance,
              * the collection view has to be instantiated like this: 
              */
-            BuildsView = new CollectionViewSource
+            var collectionViewSource = new CollectionViewSource
             {
                 Source = knownBuildsViewModel.Builds,
                 SortDescriptions = { new SortDescription(nameof(BuildInfo.BuildDefinitionName), ListSortDirection.Ascending) }
-            }.View;
+            };
+            BuildsView = collectionViewSource.View;
+
+            SortCommand = new SortCommandImpl(collectionViewSource);
+        }
+
+        public class SortCommandImpl : ICommand
+        {
+            private readonly CollectionViewSource _collectionViewSource;
+
+            public SortCommandImpl(CollectionViewSource collectionViewSource)
+            {
+                _collectionViewSource = collectionViewSource;
+            }
+
+            public CollectionView CollectionView { get; set; }
+
+            public event EventHandler CanExecuteChanged;
+            public bool CanExecute(object parameter) => true;
+
+            public void Execute(object parameter)
+            {
+                if (!(parameter is string newSort))
+                    return;
+
+                var currentSort = _collectionViewSource.SortDescriptions.First();
+                var direction = ListSortDirection.Ascending;
+
+                if (newSort == currentSort.PropertyName)
+                    direction = currentSort.Direction == ListSortDirection.Ascending ? ListSortDirection.Descending : ListSortDirection.Ascending;
+
+                _collectionViewSource.SortDescriptions.Clear();
+
+                _collectionViewSource.SortDescriptions.Add(new SortDescription(newSort, direction));
+            }
         }
     }
 }
