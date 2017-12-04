7b504fd1d546c1ff1cdaaff218e5b3fb350abac0
Michael Praschl <michael.praschl@gmail.com>
2017-12-04 21:32:50 +0100

    added explicit user (requestedby) filter

---
+++ MiP.TeamBuilds/UI/Overview/Filters/RequestedByExpressionBuilder.cs
@@ -0,0 +1,24 @@
+﻿using MiP.TeamBuilds.Providers;
+using System;
+using System.Linq.Expressions;
+using System.Collections.Generic;
+
+namespace MiP.TeamBuilds.UI.Overview.Filters
+{
+    public class RequestedByExpressionBuilder : IExpressionBuilder
+    {
+        public bool CanParse(string prefix) => prefix == "r" || prefix == "requestedBy";
+
+        public Expression<Func<BuildInfo, bool>> CreateExpression(string expression, ICollection<string> errors)
+        {
+            return buildInfo => Filter(buildInfo, expression);
+        }
+
+        private bool Filter(BuildInfo buildInfo, string requestedBy)
+        {
+            return buildInfo.RequestedByDisplayName.IndexOf(requestedBy, StringComparison.OrdinalIgnoreCase) >= 0
+                || buildInfo.RequestedBy.IndexOf(requestedBy, StringComparison.OrdinalIgnoreCase) >= 0;
+        }
+    }
+
+}
